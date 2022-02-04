733f5aa7875c4629f36e6b52f066da3b671ea6f0
Elliot Park <elliotpark410@gmail.com>
2022-02-03 23:13:40 -0800

    create db files witih schema.sql, seeds.sql, and query.sql

---
+++ db/query.sql
@@ -0,0 +1,11 @@
+USE business_db;
+
+SELECT * FROM department;
+
+-- view to see all employees by department and role 
+-- look up concat and rename column e.g. concat first_name and Last_name
+SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name, manager.first_name, manager.last_name FROM department
+INNER JOIN role ON department.id = role.department_id
+INNER JOIN employee ON role.id = employee.role_id
+LEFT JOIN employee manager ON manager.id = employee.manager_id 
+ORDER BY employee.id ASC;
---
+++ db/seeds.sql
@@ -0,0 +1,46 @@
+USE business_db;
+
+
+INSERT INTO department (name)
+VALUES ("Engineering"),
+       ("Legal"),
+       ("Finance"),
+       ("Sales");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("software engineer", 100000, 1),
+       ("lead engineer", 200000, 1),
+       ("legal assistant", 90000, 2),
+       ("legal counsel", 100000, 2),
+       ("accountant", 70000, 3),
+       ("lead accountant", 90000, 3),
+       ("sales person", 70000, 4),
+       ("sales lead", 90000, 4);
+       
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Hannah", "Folk", 2),
+       ("Elliot", "Park", 1),
+       ("Fred", "Park", 3),
+       ("Peter", "Park", 4),
+       ("Grant", "Lee", 5),
+       ("Phillip", "Park", 6),
+       ("Joseph", "Park", 7),
+       ("Hea Suk", "Park", 8);
+
+
+UPDATE employee
+SET manager_id = 1
+WHERE id = 2;
+
+UPDATE employee
+SET manager_id = 4
+WHERE id = 3;
+
+UPDATE employee
+SET manager_id = 6
+WHERE id = 5;
+
+UPDATE employee
+SET manager_id = 8
+WHERE id = 7;
